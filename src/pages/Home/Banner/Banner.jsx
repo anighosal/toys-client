7e0ef077a2db426f3a98bcd8db9e20462e3b2fa4
anighosal <121676026+anighosal@users.noreply.github.com>
2023-05-18 01:57:16 +0600

    banner added

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import banner from "../../../assets/banner.jpg";
+
+const Banner = () => {
+  return (
+    <div className="mt-5">
+      <img className="w-[1340px] h-[600px]" src={banner} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
