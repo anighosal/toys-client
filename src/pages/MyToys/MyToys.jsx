7257b3263c6bfe77cbc57b4a9eb6ac2f6c080c05
anighosal <121676026+anighosal@users.noreply.github.com>
2023-05-20 16:46:25 +0600

    delete date fetch successful

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -11,19 +11,35 @@ const MyToys = () => {
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, []);
+
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure delete this toy");
+    if (proceed) {
+      fetch(`http://localhost:5000/mytoys/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("toys deleted successfull");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
   return (
     <div>
-      <h2>my toys</h2>
+      <h2 className="text-center font-bold text-2xl text-purple-600">
+        my toys
+      </h2>
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           {/* head */}
           <thead>
             <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
+              <th>Delete</th>
               <th>Picture</th>
               <th>Name</th>
               <th>title</th>
@@ -31,12 +47,18 @@ const MyToys = () => {
               <th>Quantity</th>
               <th>Email</th>
               <th>Rating</th>
-              <th></th>
+              <th>Details</th>
+
+              <th>Update</th>
             </tr>
           </thead>
           <tbody>
             {bookings.map((booking) => (
-              <MyToysRow key={booking._id} booking={booking}></MyToysRow>
+              <MyToysRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+              ></MyToysRow>
             ))}
           </tbody>
         </table>
--- src/pages/MyToys/MyToysRow.jsx
+++ src/pages/MyToys/MyToysRow.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 
-const MyToysRow = ({ booking }) => {
-  const { customerName, email, price, subCategory, photo, title, quantity } =
-    booking;
+const MyToysRow = ({ booking, handleDelete }) => {
+  const {
+    _id,
+    customerName,
+    email,
+    price,
+    subCategory,
+    photo,
+    title,
+    quantity,
+    rating,
+    description,
+  } = booking;
   console.log(photo);
+
   return (
     <tr>
       <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="btn btn-circle btn-sm "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </th>
       <td>
         <div className="avatar">
@@ -20,11 +47,13 @@ const MyToysRow = ({ booking }) => {
       </td>
       <td>{customerName}</td>
       <td>{title}</td>
-      <td>{price}</td>
+      <td className="text-blue-500 font-bold ">$ {price}</td>
       <td>{quantity}</td>
       <td>{email}</td>
+      <td className="text-red-500 font-bold ">{rating}</td>
+      <td>{description}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        <button className="btn btn-ghost btn-xs">Update</button>
       </th>
     </tr>
   );
