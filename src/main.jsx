aa2ed84e87d8788657b9ce444e8289f54da4b604
anighosal <aasgfamily@gmail.com>
2023-12-15 01:25:39 +0600

    again jsx added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-import AuthProvider from "./Providers/AuthProvider";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
+import AuthProvider from "./providers/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="max-w-7xl mx-auto">
